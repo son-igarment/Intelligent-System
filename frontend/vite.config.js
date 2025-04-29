4f592b89ac95d16d6d67d135eb41a6cde4c97860
son-igarment <108049038+LEO2022110011@users.noreply.github.com>
2025-04-29 18:42:17 +0700

    Docker fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'chart.js': resolve(__dirname, 'node_modules/chart.js')
+    }
+  },
+  server: {
+    hmr: {
+      overlay: false
+    },
+    watch: {
+      usePolling: true
+    }
+  }
 })
